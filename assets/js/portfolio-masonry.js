cef11c0fb0850644a2a4e92d4d5b8be613ec3ac2
mark l chaves <mark@marklchaves.com>
2020-01-11 01:02:08 +0800

    Need to set columnWidth to a number
    Also added JS to only do masonry grid for screens 667px or larger.

--- assets/js/portfolio-masonry.js
+++ assets/js/portfolio-masonry.js
@@ -1,31 +1,37 @@
 // Refactored (called twice) 3 November 2019. ~mlc
 function loadMasonry() {
-    // console.log("Inside portfolio-masonry.js");
-    var grid = $('.grid__item');
-    var container = document.querySelector('.archive');
-	// Get reference to loading screen
-	var loading_screen = document.getElementById('loading');
+  if (window.screen.width >= 667) {
+    var grid = $(".grid__item");
+    var container = document.querySelector(".archive");
+    // Get reference to loading screen
+    var loading_screen = document.getElementById("loading");
     imagesLoaded(container, function() {
-        grid.fadeIn();
+      grid.fadeIn();
 
-        var msnry = new Masonry(document.querySelector('.archive'), {
-           columnWidth: 100%,
-           itemSelector: '.grid__item',
-           percentPosition: true,
-           verticalOrder: true
-        });
+      var msnry = new Masonry(document.querySelector(".archive"), {
+        columnWidth: 300,
+        itemSelector: ".grid__item",
+        percentPosition: true,
+        horizontalOrder: true
+      });
     });
-
+  }
 }
-$(window).on('load', function() {
-    loadMasonry();
+
+window.onload = function() {
+  this.loadMasonry();
+};
+
+$(window).on("load", function() {
+  loadMasonry();
 });
 
 // Added this call to fix switching to mobile view. ~mlc
-$(window).on('resize', function() {
-    loadMasonry();
+$(window).on("resize", function() {
+  loadMasonry();
 });
 
 function hideLoading(loading_screen) {
-    loading_screen.classList.add('hidden');
+  console.log("hideLoading()");
+  loading_screen.classList.add("hidden");
 }
