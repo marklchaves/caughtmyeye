180b5f0cf7d77767246185f3a716954ad84359f4
mark l chaves <mark@caughtmyeye.cc>
2020-01-11 07:52:05 +0800

    Testing orientation fix

--- assets/js/portfolio-masonry.js
+++ assets/js/portfolio-masonry.js
@@ -1,45 +1,63 @@
 // Refactored (called twice) 3 November 2019. ~mlc
 function loadMasonry() {
-  if (window.screen.width >= 1024) {
-    var grid = $(".grid__item");
-    var container = document.querySelector(".archive");
-    // Get reference to loading screen
-    var loading_screen = document.getElementById("loading");
-    imagesLoaded(container, function() {
-      grid.fadeIn();
+  var grid = $(".grid__item");
+  var container = document.querySelector(".archive");
+  // Get reference to loading screen
+  var loading_screen = document.getElementById("loading");
+  imagesLoaded(container, function() {
+    grid.fadeIn();
 
+    if (window.screen.width >= 1024) {
+      console.log("loadMasonry()");
       var msnry = new Masonry(document.querySelector(".archive"), {
         columnWidth: 300,
         itemSelector: ".grid__item",
         percentPosition: true,
         horizontalOrder: true
       });
-    });
-  }
+    }
+  });
 }
 
 window.onload = function() {
+  console.log("onload JS");
   this.loadMasonry();
 };
 
+window.onresize = function() {
+  console.log("onresize JS");
+  this.loadMasonry();
+};
+
+/*
 $(window).on("load", function() {
+  console.log("onload JQ");
   loadMasonry();
 });
+*/
 
 // Added this call to fix switching to mobile view. ~mlc
+/*
 $(window).on("resize", function() {
+  console.log("resize");
   loadMasonry();
 });
+*/
 
+// Don't know what this is for. ~mlc
 function hideLoading(loading_screen) {
   console.log("hideLoading()");
   loading_screen.classList.add("hidden");
 }
 
-window.addEventListener('orientationchange', function () {
-  var originalBodyStyle = getComputedStyle(document.body).getPropertyValue('display');
-  document.body.style.display='none';
-  setTimeout(function () {
+// Fix for orientation switching. ~mlc
+window.addEventListener("orientationchange", function() {
+  console.log("orientationchange");
+  var originalBodyStyle = getComputedStyle(document.body).getPropertyValue(
+    "display"
+  );
+  document.body.style.display = "none";
+  setTimeout(function() {
     document.body.style.display = originalBodyStyle;
   }, 10);
 });
