616ca6ece1a3dcbde9a5ec97342a8f493ff3af5e
mark l chaves <mark@caughtmyeye.cc>
2020-02-02 16:55:22 +0800

    Cleaned up masonry again
    Followed vanilla JS imagesLoaded CodePen https://codepen.io/desandro/pen/MwJoLQ.

--- assets/js/portfolio-masonry.js
+++ assets/js/portfolio-masonry.js
@@ -1,22 +1,23 @@
-// Refactored (called twice) 3 November 2019. ~mlc
 function loadMasonry() {
-  var grid = $(".grid__item");
-  var container = document.querySelector(".archive");
-  // Get reference to loading screen. Research what this is supposed to do. ~mlc
-  var loading_screen = document.getElementById("loading");
-  imagesLoaded(container, function() {
-    grid.fadeIn();
 
-    // Let's only do the masonry for large displays that are not on Safari.
-    if ((document.body.clientWidth >= 1024) && (navigator.userAgent.indexOf("Safari") == -1)) {
-      var msnry = new Masonry(document.querySelector(".archive"), {
-        columnWidth: 300,
-        gutter: 10,
-        itemSelector: ".grid__item",
-        percentPosition: true,
-        horizontalOrder: true
-      });
-    }
+  // Let's only do the masonry for large displays.
+  if (document.body.clientWidth < 1024) {
+    return;
+  }
+
+  var grid = document.querySelector('.archive');
+  var msnry;
+
+  // Note: there's a known bug with this for Safari.
+  imagesLoaded( grid, function() {
+    // init Isotope after all images have loaded
+    msnry = new Masonry( grid, {
+      columnWidth: 300,
+      gutter: 10,
+      itemSelector: ".grid__item",
+      percentPosition: true,
+      horizontalOrder: true
+    });
   });
 }
 
@@ -29,12 +30,5 @@ window.onresize = function() {
 };
 
 window.onorientationchange = function() { 
-  // window.location.reload();
-  // Try to reload masonry only.
   this.loadMasonry();
-}
-
-// Don't know what this is for. ~mlc
-function hideLoading(loading_screen) {
-  loading_screen.classList.add("hidden");
 }
